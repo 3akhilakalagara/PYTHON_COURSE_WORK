5672808d2923e9d712c6e3753c772ffb8f780389
3akhilakalagara <akhilakalagara45@gmail.com>
2025-07-11 11:00:50 +0530

    assignment

--- ASSIGNMENTS/ASSIGNMENT 1.py
+++ ASSIGNMENTS/ASSIGNMENT 1.py
@@ -2,43 +2,41 @@
 
 """DATA TYPES"""
 
-# 1.integer
-num_of_cart_items = 3
+#str
+product = input("Enter name of the product: ") #Enter name of the product: pilgrim face serum
+print(f"Product: {product}") #Product: pilgrim face serum
 
-#2.float
-total_order_value = 599.50
+#float
+price = float(input("Enter the price of the product: ")) #Enter the price of the product: 699.80
+print(f"Price: {price}") #Price: 699.80
 
-#3.string
-customer_name = "Akhila"
-address = "Ameerpet, Hyderabad"
+#int
+quantity = int(input("Enter the quantity of the product: ")) #Enter the quantity of the product: 1
+print(f"Quantity: {quantity}") #Quantity: 1
 
-#4.list[mutable]
-cart_items = ["Plum Facewash", "Deconstruct Suncreen", "L'Oreal conditioner"]
-wishlist_items = ["Mascara", "Lipstick", "Perfume"]
+#list
+wishlist_items = list(map(str, input("Enter the items in your wishlist: ").split()))
+print(f"Wishlist Items: {wishlist_items}") #Wishlist Items: ['serum', 'shampoo', 'conditioner']
 
-#5.tuple[immutable]
-customer_info = ("Akhila", "akhilakalagara@gmail.com", "9876543210")
+#tuple
+profile = tuple(map(str, input("Enter your profile details (name, email, phone): ").split()))
+print(f"Profile: {profile}")
+"""Enter your profile details (name, email, phone): akhila akhila@gmail.com 98765432
+Profile: ('akhila', 'akhila@gmail.com', '98765432')"""
 
-#6.dictionary[key:value pairs]
-customer_preferences = {
-    "skin_type": "oily",
-    "preferred_brands": ["Plum", "Deconstruct", "L'Oreal"]
-}
+#set
+categories = set(map(str, input("Enter the categories you browsed: ").split()))
+print(f"Categories: {categories}")
+"""Enter the categories you browsed: skincare makeup haircare
+Categories: {'skincare', 'makeup', 'haircare'}"""
 
-#7.set[unordered collection of unique items]
-categories_browsed = {"skincare", "makeup", "haircare"}
+#dict
+customer_preferences = eval(input("Enter your preferences: ")) 
+print(f"Customer Preferences: {customer_preferences}")
+"""Enter your preferences: {'skin type' : 'oily', 'brand':'pilgrim'}
+Customer Preferences: {'skin type': 'oily', 'brand': 'pilgrim'}"""
 
-#8.boolean
+#boolean
 is_premium_member = True
-
-
-print("Customer Name:", customer_name) #Customer Name: Akhila
-print("Number of Cart Items:", num_of_cart_items)# Number of Cart Items: 3
-print("Total Order Value:", total_order_value)# Total Order Value: 599.50
-print("Shipping Address:", address)# Shipping Address: Ameerpet, Hyderabad
-print("Cart Items:", cart_items)# Cart Items: ['Plum Facewash', 'Deconstruct Suncreen', "L'Oreal conditioner"]
-print("Wishlist Items:", wishlist_items)# Wishlist Items: ['Mascara', 'Lipstick', 'Perfume']
-print("Customer Info:", customer_info)# Customer Info: ('Akhila', 'akhilakalagara@gmail.com', '9876543210')
-print("Customer Preferences:", customer_preferences)# Customer Preferences: {'skin_type': 'oily', 'preferred_brands': ["Plum", "Deconstruct", "L'Oreal"]}
-print("Categories Browsed:", categories_browsed)# Categories Browsed: {'skincare', 'makeup', 'haircare'}
 print("Is Premium Member:", is_premium_member)# Is Premium Member: True
+
